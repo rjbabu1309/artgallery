b9c6968e1b655d197c106eb593814add06adbb92
rjbabu1309 <rjbabu1309@gmail.com>
2021-12-23 21:01:08 +0530

    first commit

--- src/components/partials/header/Header.js
+++ src/components/partials/header/Header.js
@@ -14,8 +14,8 @@ const Header = () => {
       <Nav.Link as={Link} to='gallery'>Gallery</Nav.Link>
       <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
       <Nav.Link as={Link} to='/order'>Order Now</Nav.Link>
-      {/* <Nav.Link as={Link} to='/about'>About</Nav.Link> */}
       <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+      <Nav.Link href='https://www.instagram.com/alleam_20/' target='_blank'>Instagram</Nav.Link>
       {/* <Nav.Link eventKey={2} href="#memes">
         Dank memes
       </Nav.Link> */}
