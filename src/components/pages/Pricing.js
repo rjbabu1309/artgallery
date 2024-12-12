5d1d1435baadb3a666820f526e522979ef5e13b7
Raja Babu <35734506+rjbabu1309@users.noreply.github.com>
2024-12-13 01:29:23 +0530

    price update

--- src/components/pages/Pricing.js
+++ src/components/pages/Pricing.js
@@ -5,8 +5,8 @@ import ProductCard from "../cards/ProductCard";
 const Pricing = () => {
     return (
         <Container>
-        <ProductCard title={'Without Frame'} price={400} url={'/contact'} imgSrc="art_wo_frame.jpg"> </ProductCard>
-        <ProductCard title={'With Frame'} price={800} url={'/contact'} imgSrc="art_w_frame.jpg"></ProductCard>
+        <ProductCard title={'Without Frame'} price={500} url={'/contact'} imgSrc="art_wo_frame.jpg"> </ProductCard>
+        <ProductCard title={'With Frame'} price={1000} url={'/contact'} imgSrc="art_w_frame.jpg"></ProductCard>
         </Container>
     );
 }
